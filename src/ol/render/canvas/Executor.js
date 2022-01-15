a48258c101852acdf4f0a6d680bef04d5facaa1c
José M. Pérez <JMPerez@users.noreply.github.com>
2022-01-15 21:22:20 +0100

    Update Executor.js

--- src/ol/render/canvas/Executor.js
+++ src/ol/render/canvas/Executor.js
@@ -80,25 +80,13 @@ function getDeclutterBox(replayImageOrLabelArgs) {
   return replayImageOrLabelArgs[3].declutterBox;
 }
 
-const rtlRegEx = new RegExp(
-  /* eslint-disable prettier/prettier */
-  '[' +
-    String.fromCharCode(0x00591) + '-' + String.fromCharCode(0x008ff) +
-    String.fromCharCode(0x0fb1d) + '-' + String.fromCharCode(0x0fdff) +
-    String.fromCharCode(0x0fe70) + '-' + String.fromCharCode(0x0fefc) +
-    String.fromCharCode(0x10800) + '-' + String.fromCharCode(0x10fff) +
-    String.fromCharCode(0x1e800) + '-' + String.fromCharCode(0x1efff) +
-  ']'
-  /* eslint-enable prettier/prettier */
-);
-
 /**
  * @param {string} text Text.
  * @param {string} align Alignment.
  * @return {number} Text alignment.
  */
 function horizontalTextAlign(text, align) {
-  if ((align === 'start' || align === 'end') && !rtlRegEx.test(text)) {
+  if ((align === 'start' || align === 'end')) {
     align = align === 'start' ? 'left' : 'right';
   }
   return TEXT_ALIGN[align];
